428415cd2b341a3e4b670aa44859aa3d537a405e
Shahmar700 <shahmar700@gmail.com>
2022-07-07 12:34:21 +0400

    first

---
+++ app.js
@@ -0,0 +1,87 @@
+const startBtn = document.getElementById("start");
+const scoreText = document.getElementById("score");
+const moles = document.querySelectorAll(".mole");
+const timeText = document.getElementById("time");
+const timeSection = document.getElementById("timeSection");
+
+let prevMole;
+let timeIsUp = false;
+let score = 0;
+scoreText.textContent = score;
+let time = 15;
+timeText.textContent = time;
+
+const randomMole = () => {
+  const order = Math.floor(Math.random() * moles.length);
+  const selectedMole = moles[order];
+  if (prevMole == selectedMole) {
+    return randomMole();
+  } else {
+    prevMole = selectedMole;
+  }
+  return selectedMole;
+};
+
+const randomTime = (min, max) => {
+  const time = Math.round(Math.random() * (max - min) + min);
+  return time;
+};
+
+const up = () => {
+  const mole = randomMole();
+  const upTime = randomTime(750, 1000);
+  mole.classList.add("selected");
+  setTimeout(() => {
+    mole.classList.remove("selected");
+    if (!timeIsUp) {
+      up();
+    }
+  }, upTime);
+};
+
+const startTime = () => {
+  if (!timeIsUp) {
+    if (time < 4) {
+      timeSection.classList.add("red");
+      timeText.classList.add("scale");
+    }
+    time--;
+    timeText.textContent = time;
+  } else {
+    timeText.textContent = "Time Is Up!";
+    startBtn.disabled = false;
+  }
+};
+
+const startGame = () => {
+  time = 15;
+  score = 0;
+  timeIsUp = false;
+  const interval = setInterval(() => {
+    startTime();
+    if (timeIsUp) clearInterval(interval);
+  }, 1000);
+
+  up();
+  setTimeout(() => {
+    timeIsUp = true;
+  }, 15000);
+};
+
+const hit = (e) => {
+  if (e.target.classList.contains("selected")) {
+    score++;
+    e.target.classList.remove("selected");
+  }
+  scoreText.textContent = score;
+};
+
+moles.forEach((mole) => {
+  mole.addEventListener("click", hit);
+});
+
+startBtn.addEventListener("click", () => {
+  startGame();
+  scoreText.textContent = 0;
+  startBtn.disabled = true;
+});
